cb3719ad680aa6447c6ab72debf7a07d1e542923
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2024-11-24 18:19:46 -0300

    Atividade 02 inicio

---
+++ rest-api-zaruc/src/test/java/com/example/rest_api_zaruc/RestApiZarucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_zaruc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiZarucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
