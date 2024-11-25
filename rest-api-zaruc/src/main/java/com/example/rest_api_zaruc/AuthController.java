41191c1213d53899e53bc11b3dd13fca2ac9ad92
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2024-11-24 23:45:00 -0300

    Criação do CRUD, Criação da imagem do Docker. Não consegui testar ainda

---
+++ rest-api-zaruc/src/main/java/com/example/rest_api_zaruc/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.rest_api_zaruc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/users")
+    public ResponseEntity<?> login(@RequestBody LoginRequest authRequest) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+        String token = jwtUtil.generateToken(authRequest.getUsername());
+
+
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
---
+++ rest-api-zaruc/src/main/java/com/example/rest_api_zaruc/User.java
@@ -0,0 +1,38 @@
+package com.example.rest_api_zaruc;
+
+public class User {
+    private String nome, email, senha;
+    private Long id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
